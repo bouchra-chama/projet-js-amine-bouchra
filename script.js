f7e1145441c4a6db9cf26ab8d5d54d8208981ff2
Bouchra <elmoutaqiboushra2016@gmail.com>
2021-03-24 16:07:29 +0100

    projet javascript

--- script.js
+++ script.js
@@ -1,57 +1,67 @@
-var messagesCount = document.getElementsByTagName('p').length;
-document.getElementById('count').textContent = messagesCount;
+var buttonAdd = document.getElementById('btn-add');
+var inputMessage = document.getElementById('add-message');
+var buttonTrash = document.getElementsByClassName('trash');
 
-for(var i=0; i<document.getElementsByClassName('trash').length; i++){
-    document.getElementsByClassName('trash')[i].addEventListener("click",
-    function(){
-        this.parentNode.remove();
-        var messagesCount = document.getElementsByTagName('p').length;
-        document.getElementById('count').textContent = messagesCount;
+buttonAdd.addEventListener('click', function(){
+    var row = document.createElement('div');
+    row.classList.add('row');
+
+    var img = document.createElement('img');
+    img.setAttribute('src', 'avatar-2.jpg');
+    img.classList.add('avatar');
+    row.appendChild(img);
+
+    var wrapper = document.createElement('div');
+    var title = document.createElement('h6');
+    title.textContent = 'Bouchra';
+    wrapper.appendChild(title);
+
+    var p = document.createElement('p');
+    p.textContent = inputMessage.value;
+    wrapper.appendChild(p);
+
+    row.appendChild(wrapper);
+
+    var imgTrash = document.createElement('img');
+    imgTrash.setAttribute('src', 'trash.png');
+    imgTrash.classList.add('trash');
+    row.appendChild(imgTrash);
+
+    document.body.appendChild(row);
+
+    inputMessage.value = '';
+
+    // mise à jour du compteur
+    majCompteurMsg('add');
+
+    // ajout event listener click sur l'element que je viens de créer
+    removeBloc();
+
+});
+
+removeBloc();
+majCompteurMsg();
+
+function removeBloc() {
+    for(var i=0; i<buttonTrash.length; i++) {
+        buttonTrash[i].addEventListener('click', function(){
+            this.parentNode.remove();
+
+            // mise à jour du compteur
+            majCompteurMsg('remove');
+        });
     }
-    )
-
 }
 
-document.getElementById('btn-add').addEventListener("click",
-    function(){
-        var mainDiv = document.createElement("div");
-            mainDiv.className = "row";
-            document.body.appendChild(mainDiv);
-
-        var image = document.createElement("img");
-            image.className = "avatar";
-            image.src = "avatar-1.jpg";
-            mainDiv.appendChild(image);
-        
-        var nameMessageDiv = document.createElement("div");
-            mainDiv.appendChild(nameMessageDiv);
-
-        var h6 = document.createElement('h6');
-            h6.textContent = "Eric Dupont";
-            nameMessageDiv.appendChild(h6);
-
-        var content = document.createElement('p');
-            content.textContent = document.getElementById('add-message').value;
-            nameMessageDiv.appendChild(content);
-
-        var trash = document.createElement('img');
-        trash.src = "trash.png";
-        trash.className = "trash";
-        mainDiv.appendChild(trash);
-
-        document.getElementById('add-message').value = "";
-        
-        var messagesCount = document.getElementsByTagName('p').length;
-        document.getElementById('count').textContent = messagesCount;
-
-        
-        trash.addEventListener("click", 
-            function(){
-                this.parentNode.remove();
-                var messagesCount = document.getElementsByTagName('p').length;
-                document.getElementById('count').textContent = messagesCount;
-
-            }
-        )
+function majCompteurMsg(param) {
+    var count = document.getElementById('count');
+    if(param == 'add') {
+        count.textContent = parseInt(count.textContent) + 1;
     }
-)
+    else if(param == 'remove') {
+        count.textContent = parseInt(count.textContent) - 1;
+    }
+    else {
+        count.textContent = document.getElementsByClassName('row').length - 2; // - 2 car il y a 2 div au début qu'il ne faut pas compté
+    }
+}
